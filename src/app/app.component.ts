a258b5b14b3f2c836eea7abed2b44c3a3caa3760
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-08 06:10:29 +0800

    Removed title method from component in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<p>Buongiorno!</p>`,
   standalone: true,
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // styles: ['']
 })
 export class AppComponent {
   name = signal('Brooke');
