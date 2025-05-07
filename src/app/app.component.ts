ea6b4380e2c9e0f870523d65730e0331a75ff61a
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-08 06:57:38 +0800

    Added image to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, signal } from '@angular/core';
 })
 export class AppComponent {
   name = signal('Brooke');
+  imageURL = signal('https://picsum.photos/200/300?grayscale');
 
   getName() {
     return this.name();
