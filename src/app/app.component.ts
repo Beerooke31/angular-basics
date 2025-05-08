5cfd5988dbb66d591cb88fe388aed712e028db0f
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-08 20:44:27 +0800

    Experimented with lifecycle hooks and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component, signal } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  imports: [PostComponent],
 })
 export class AppComponent {
   name = signal('Brooke');
-  imageURL = signal('https://picsum.photos/200/300?grayscale');
+  imageURL = signal(
+    'https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ'
+  );
 
   getName() {
     return this.name();
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imageURL.set((e.target as HTMLInputElement).value);
+  }
+
+  logImage(e: string) {
+    console.log(e);
   }
 }
