3d92af31ecd76f66ea2b94edfe1ff2e6e40c49c9
Brooke <133279077+Beerooke31@users.noreply.github.com>
2025-05-09 20:27:00 +0800

    Added directives to app (I realise this exercise doesn't look great for a GitHub repo, but hey, learning is a journey.)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,36 @@
 import { Component, signal } from '@angular/core';
 import { PostComponent } from './post/post.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [PostComponent],
+  imports: [PostComponent, CommonModule],
 })
 export class AppComponent {
-  name = signal('Brooke');
+  name = signal('brooke');
   imageURL = signal(
     'https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ'
   );
+  images = signal([
+    'https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ',
+    'https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ',
+    'https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ',
+  ]);
+  currentDate = signal(new Date());
+  cost = signal(2000);
+  temperature = signal(25.3);
+  message = signal('Hello this is my message.');
+  count = signal(0);
+  pizza = signal({
+    toppings: ['stracciatella', 'truffle'],
+    size: 'large',
+  });
+
+  blueClass = signal(false);
+  fontSize = signal(16);
 
   getName() {
     return this.name();
